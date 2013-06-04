ee9caeba1c0c706cbfe4a4b362a8467b675500ac
mbelhadj <mbelhadj@excilys.com>
2013-06-04 11:56:17 +0200

    merge tiles computer database and master

--- src/main/java/com/excilys/service/ParamService.java
+++ src/main/java/com/excilys/service/ParamService.java
@@ -1,45 +1,55 @@
 package com.excilys.service;
 
+import com.excilys.utils.AttributeUtils;
+
 public enum ParamService {
-	
+
 	INSTANCE;
-	
+
 	private String order;
 	private String by;
 	private String search;
-	private String current;
+	private AttributeUtils current = new AttributeUtils("", "");
 	private int currentCount;
-	
+
 	public String getOrder() {
 		return order;
 	}
+
 	public void setOrder(String order) {
 		this.order = order;
 	}
+
 	public String getBy() {
 		return by;
 	}
+
 	public void setBy(String by) {
 		this.by = by;
 	}
+
 	public String getSearch() {
 		return search;
 	}
+
 	public void setSearch(String search) {
 		this.search = search;
 	}
-	public String getCurrent() {
+
+	public AttributeUtils getCurrent() {
 		return current;
 	}
-	public void setCurrent(String current) {
+
+	public void setCurrent(AttributeUtils current) {
 		this.current = current;
 	}
+
 	public int getCurrentCount() {
 		return currentCount;
 	}
+
 	public void setCurrentCount(int currentCount) {
 		this.currentCount = currentCount;
 	}
-	
-	
+
 }
