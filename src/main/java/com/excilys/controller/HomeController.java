2fa4b16edcf8f24d783d64c6bff12528c5d30d48
mbelhadj <mbelhadj@excilys.com>
2013-06-05 16:02:07 +0200

    Added UrlBind

--- src/main/java/com/excilys/controller/HomeController.java
+++ src/main/java/com/excilys/controller/HomeController.java
@@ -9,8 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
+import com.excilys.controller.form.UrlBind;
 import com.excilys.om.Computer;
 import com.excilys.service.ComputerService;
 import com.excilys.utils.ErrorUtils;
@@ -19,45 +19,30 @@ import com.excilys.utils.SortUtils;
 @Controller
 public class HomeController {
 
-	private static final String FILTRE_SEARCH = "f";
-	private static final String FILTRE_ORDER = "o";
-	private static final String FILTRE_BY = "s";
-	private static final String PAGINATION_LEFT_BOOL = "r";
-	private static final String PAGINATION = "p";
 	private static final String SORT_SERVICE = "ss";
 	private static final String UTILS_SERVICE = "us";
 	private static final String LIST_COMPUTERS = "lc";
+	private static final String PAGINATION = "p";
 
 	@Autowired
 	private ComputerService computerService;
 
 	@RequestMapping(value = "/computersDis", method = RequestMethod.GET)
-	public String home(
-			Model m,
-			HttpSession session,
-			@RequestParam(value = PAGINATION, defaultValue = "-1") int p,
-			@RequestParam(value = PAGINATION_LEFT_BOOL, defaultValue = "false") boolean r,
-			@RequestParam(value = FILTRE_BY, defaultValue = "c.name") String by,
-			@RequestParam(value = FILTRE_ORDER, defaultValue = "DESC") String order,
-			@RequestParam(value = FILTRE_SEARCH, defaultValue = "") String search) {
+	public String home(Model m, HttpSession session, UrlBind urlBind) {
 
 		ErrorUtils utilsService = ErrorUtils.init(session);
 		SortUtils sortService = SortUtils.init(session);
-
+		int p = urlBind.getP();
 		if (p != -1) {
-			if (r != false)
-				p--;
-			else
-				p++;
+			p = urlBind.moveP();
 		} else {
 			p = 0;
 			sortService.defaultSet();
-			sortService.setPs(by, order, search);
+			sortService.setPs(urlBind.getS(), urlBind.getO(), urlBind.getF());
 			sortService.setCurrentCount(computerService.getCurrentCount(p,
 					sortService.getReq(), sortService.getPs().getBy(),
 					sortService.getPs().getSearch()));
 		}
-
 		List<Computer> lc = computerService.findOrderByComputers(p, sortService
 				.getReq(), sortService.getPs().getBy(), sortService.getPs()
 				.getSearch());
@@ -68,4 +53,6 @@ public class HomeController {
 		m.addAttribute(LIST_COMPUTERS, lc);
 		return "computer";
 	}
+
+
 }
